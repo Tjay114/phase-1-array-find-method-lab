e4c1cacb77dc97d0fd656b140178e4102a2a077a
Terormumuli <terahmumuli@gmail.com>
2022-06-14 07:39:39 +0300

    Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const superbowlWin = (record) => {
+    const cb =  (element) => {
+        return element.result === 'W'
+    }
+
+    const winYear = record.find(cb)
+
+    if (winYear){
+        return winYear.year
+    }else {
+        return undefined
+    }
+}
